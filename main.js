701da23a2f4d6837134768b3c1e4d6bd08242946
Jason Wiguna <jasonwiguna@Jasons-MacBook-Pro.local>
2023-07-19 21:17:23 +0700

    Add licensing logic

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const si = require('systeminformation');
+
+let hid = '';
+
+si.system().then(data => {
+  hid = data.uuid;
+}).catch(error => console.error(error));
  
+let mainWindow
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 
 function createWindow () {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 1400,
     height: 800,
     minWidth: 1400,
@@ -23,16 +33,43 @@ function createWindow () {
     preload: path.join(__dirname, 'preload.js')
   }})
   // win.webContents.openDevTools();
-  win.loadFile(path.join(__dirname, 'resources/views/index.html'));
+  mainWindow.loadFile(path.join(__dirname, 'resources/views/index.html'));
+}
+
+async function createBackgroundTask () {
+  while(true) {
+    const myBody = {
+        hid
+    }
+    const response = await Promise.race([
+      fetch('http://db.vstream.asia/backend/validateHid', {
+          method: 'POST',
+          body: JSON.stringify(myBody),
+          headers: {
+          'Content-Type': 'application/json'
+          }
+      }),
+      new Promise((_, reject) =>
+          setTimeout(() => reject(mainWindow.loadFile(path.join(__dirname, 'resources/views/index.html'))), 5000)
+      )
+    ]);
+    const res = await response.json();
+    if (!res.success) {
+      mainWindow.loadFile(path.join(__dirname, 'resources/views/index.html'));
+    }
+    await delay(24*60*60*1000);
+  }
 }
 
  
 app.whenReady().then(() => {
   createWindow()
+  createBackgroundTask()
   //Untuk Mac os perlu melakukan create Window saat active
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-     createWindow()
+      createWindow()
+      createBackgroundTask()
     }
   });
 });
@@ -42,4 +79,4 @@ app.on('window-all-closed', () =>{
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
+});
--- preload.js
+++ preload.js
@@ -1,13 +1,22 @@
+const si = require('systeminformation');
+
+let hid = '';
+
+si.system().then(data => {
+  hid = data.uuid;
+}).catch(error => console.error(error));
+
 window.addEventListener('DOMContentLoaded', () => {
+  localStorage.setItem("hid", hid)
 
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text;
-    }
-   
-    for (const type of ['chrome', 'node', 'electron', 'require']) {
-      replaceText(`${type}-version`, process.versions[type])
-    }
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text;
+  }
+  
+  for (const type of ['chrome', 'node', 'electron', 'require']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
 
-  })
+})
   
